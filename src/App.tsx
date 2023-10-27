1a675f1e058094710ce9e0436c61e8ef870aeb67
Carlos David Apolaya Mendoza <apolayamendozacarlos@gmail.com>
2023-10-27 13:34:35 -0500

    Se agrega Linter al proyecto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const App = () => {
-
+const App = (): JSX.Element => {
   const [mario, setMario] = useState([])
 
   const ApiUrl = import.meta.env.VITE_API_URL
 
   useEffect(() => {
     axios.get(ApiUrl)
-    .then(datos => [
-      setMario(datos.data.amiibo)
-    ])
-    .catch(() => console.error("Falla"))
-  },[])
+      .then(datos => [
+        setMario(datos.data.amiibo)
+      ])
+      .catch(() => { console.error('Falla') })
+  }, [])
 
-  const charactersRendered = new Set();
+  const charactersRendered = new Set()
 
-  return(
+  return (
     <div className="bg-gray-900">
       <div className="grid grid-cols-2 gap-4 container mx-auto">
-        {mario.map(({tail, character, image }) => {
-          
+        {mario.map(({ tail, character, image }) => {
           if (charactersRendered.has(character)) {
-            return null;
+            return null
           }
 
-          charactersRendered.add(character);
+          charactersRendered.add(character)
 
           return (
             <div key={tail}>
               <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <img className="w-60 h-60 object-cover object-center rounded-t-lg md:rounded-none md:rounded-l-lg" 
+              <img className="w-60 h-60 object-cover object-center rounded-t-lg md:rounded-none md:rounded-l-lg"
                 src={image} alt=""/>
                   <div className="flex flex-col justify-between p-4 leading-normal">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> 
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         {character}
                       </h5>
                       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
@@ -43,7 +41,7 @@ const App = () => {
                   </div>
               </div>
             </div>
-          );
+          )
         })}
       </div>
     </div>
